887fb8106d5d26e48f54d9ac0cac5a20154a03f1
Zeiyar Lynn <zeiyarlynn@gmail.com>
2022-01-16 16:39:44 +0630

    completed mobile view for About component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -26,6 +26,18 @@ function About() {
             descr="You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you'll be added to a special Backer member list."
             amount="101"
           />
+          <PriceCard 
+            itemName="Black Edition Stand"
+            restriction="pledge $75 or more"
+            descr="You get a Black Special Edition computer stand and a personal thank you. You'll be added to our Backer member list. Shipping is included"
+            amount="64"
+          />
+          <PriceCard
+            itemName="Mahogany Special Edition"
+            restriction="Pledge $200 or more"
+            descr="You get two Special Edition Mahogany stands, a Backer T0Shirt, and a personal thank you. You'll be added to our Backer member list. Shipping is included"
+            amount="0"
+          />          
         </div>
       </Container>
     </section>
