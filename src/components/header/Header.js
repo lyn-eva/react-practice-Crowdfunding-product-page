d8bed019b5751831a34e87e798f663bb3a91c13d
Zeiyar Lynn <zeiyarlynn@gmail.com>
2022-01-16 11:51:10 +0630

    fixed menu icon

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,26 @@
-import React from "react";
+import {useState} from "react";
 
 import logo from "../../assests/logo.svg";
 import menuIcon from "../../assests/icon-hamburger.svg"
+import closeIcon from "../../assests/icon-close-menu.svg"
 import styles from "./Header.module.css"
 import Container from "../UI/Container";
 import Card from "../UI/Card";
 
 function Header() {
+  const [menuDropped, setMenuDropped] = useState(false);
+
   const clickHandler = e => {
     e.preventDefault();
+    setMenuDropped(prevState => !prevState);
   }
 
   return (
     <header className={styles.header}>
       <Container className={styles['nav-bar']}>
         <div><img src={logo} alt="crowdfund"/></div>
-        <nav>
-          <a href="" onClick={clickHandler} id={styles['action-btn']}><img src={menuIcon} alt="menuIcon"/></a>
+        <nav className={menuDropped ? styles['menu-dropped'] : ''}>
+          <a href="" onClick={clickHandler} id={styles['action-btn']}><img src={menuDropped ? closeIcon : menuIcon} alt="menuIcon"/></a>
           <Card className={styles['drop-menu']}>
             <ul>
               <li><a href=''>About</a></li>
